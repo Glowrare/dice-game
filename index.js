5f75ed08778035780ee186d87bbbd9a5b7854e9c
Ajibolanle Gloria <aajibolanle@yahoo.com>
2020-09-24 21:23:58 +0100

    code reworked

--- index.js
+++ index.js
@@ -1,55 +1,20 @@
-let randomNumber1 = (Math.floor(Math.random() * 6)) + 1;
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+let dicePicker1 = "dice" + randomNumber1 + ".png";
+let dicePicker2 = "dice" + randomNumber2 + ".png";
 
-const diceGallery = [
-  "images/dice1.png",
-  "images/dice2.png",
-  "images/dice3.png",
-  "images/dice4.png",
-  "images/dice5.png",
-  "images/dice6.png",
-];
+document
+  .querySelectorAll("img")[0]
+  .setAttribute("src", `images/${dicePicker1}`);
 
-function player1() {
-  let result1 = diceGallery[randomNumber1 - 1];
-  document.querySelectorAll("img")[0].setAttribute("src", result1);
+document
+  .querySelectorAll("img")[1]
+  .setAttribute("src", `images/${dicePicker2}`);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!🤝🏾";
 }
-
-function player2() {
-  let result2 = diceGallery[randomNumber1 - 1];
-  document.querySelectorAll("img")[1].setAttribute("src", result2);
-}
-
-// const dice1 = document
-//   .querySelectorAll("img")[0]
-//   .setAttribute("src", "images/dice1.png");
-// const dice2 = document
-//   .querySelectorAll("img")[0]
-//   .setAttribute("src", "images/dice2.png");
-// const dice3 = document
-//   .querySelectorAll("img")[0]
-//   .setAttribute("src", "images/dice3.png");
-// const dice4 = document
-//   .querySelectorAll("img")[0]
-//   .setAttribute("src", "images/dice4.png");
-// const dice5 = document
-//   .querySelectorAll("img")[0]
-//   .setAttribute("src", "images/dice5.png");
-// const dice6 = document
-//   .querySelectorAll("img")[0]
-//   .setAttribute("src", "images/dice6.png");
-
-// function dicePicker() {
-//   if (randomNumber1 === 1) {
-//     dice1;
-//   } else if (randomNumber1 === 2) {
-//     dice2;
-//   } else if (randomNumber1 === 3) {
-//     dice3;
-//   } else if (randomNumber1 === 4) {
-//     dice4;
-//   } else if (randomNumber1 === 5) {
-//     dice5;
-//   } else if (randomNumber1 === 6) {
-//     dice6;
-//   }
-// }
